cd8e8afb90d8c70acea204775da5fb0932d7c897
pintu sharma <pin2sharma248@gmail.com>
2021-06-08 21:41:02 +0900

    docker entrypoint

---
+++ docker-entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# update your 'HOSTNAME' 'USERNAME' 'PASSWORD'
+# 'DATABASE_NAME' 'TABLE_NAME' values
+
+python /root/data_injection.py 'HOSTNAME' 'USERNAME' 'PASSWORD' 'DATABASE_NAME' 'TABLE_NAME'
